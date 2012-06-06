20a6d4c46f26214c9a6eeac717acd3c36b2b9652
Andrea Vedaldi <vedaldi@gmail.com>
2012-06-06 19:18:04 +0100

    exercise1: minor refinment

--- exercise1.m
+++ exercise1.m
@@ -15,17 +15,22 @@ im2 = imread('data/oxbuild_images/all_souls_000015.jpg') ;
 [frames1, descrs1] = getFeatures(im1, 'peakThreshold', 0.001) ;
 [frames2, descrs2] = getFeatures(im2, 'peakThreshold', 0.001) ;
 
-figure(1) ; imagesc(im1) ; axis equal off ; hold on ;
+figure(1) ;
+subplot(1,2,1) ; imagesc(im1) ; axis equal off ; hold on ;
 vl_plotframe(frames1, 'linewidth', 2) ;
 
-figure(2) ; imagesc(im2) ; axis equal off ; hold on ;
+subplot(1,2,2) ; imagesc(im2) ; axis equal off ; hold on ;
 vl_plotframe(frames2, 'linewidth', 2) ;
 
 % Rotate the second image
 im3 = imrotate(im2,35,'bilinear') ;
-frames3 = getFeatures(im3, 'peakThreshold', 0.001) ;
+[frames3, descrs3] = getFeatures(im3, 'peakThreshold', 0.001) ;
 
-figure(3) ; imagesc(im3) ; axis equal off ; hold on ;
+figure(2) ;
+subplot(1,2,1) ; imagesc(im2) ; axis equal off ; hold on ;
+vl_plotframe(frames2, 'linewidth', 2) ;
+
+subplot(1,2,2) ; imagesc(im3) ; axis equal off ; hold on ;
 vl_plotframe(frames3, 'linewidth', 2) ;
 
 % --------------------------------------------------------------------
@@ -33,7 +38,7 @@ vl_plotframe(frames3, 'linewidth', 2) ;
 % --------------------------------------------------------------------
 
 % Visualize SIFT descriptors (only a few)
-figure(4) ; clf ; imagesc(im1) ; axis equal off ;
+figure(3) ; clf ; imagesc(im1) ; axis equal off ;
 vl_plotsiftdescriptor(descrs1(:,1:50:end), ...
                       frames1(:,1:50:end)) ;
 
@@ -45,8 +50,8 @@ nn = findNeighbours(descrs1, descrs2) ;
 matches = [1:size(descrs1,2) ; nn(1,:)] ;
 
 % Display the matches
-figure(5) ; clf ;
-plotmatches(im1,im2,frames1,frames2,matches) ;
+figure(4) ; clf ;
+plotMatches(im1,im2,frames1,frames2,matches) ;
 title('Nearest neighbour matches') ;
 
 % --------------------------------------------------------------------
@@ -57,16 +62,16 @@ title('Nearest neighbour matches') ;
 [nn, dist2] = findNeighbours(descrs1, descrs2, 2) ;
 
 % Accept neighbours if their second best match is sufficiently far off
-nnThreshold = 1.1 ;
-ratio2 = dist2(2,:) ./ dist2(1,:) ;
-ok = ratio2 > nnThreshold^2 ;
+nnThreshold = 0.8 ;
+ratio2 = dist2(1,:) ./ dist2(2,:) ;
+ok = ratio2 <= nnThreshold^2 ;
 
 % Construct a list of filtered matches
 matches_2nn = [find(ok) ; nn(1, ok)] ;
 
 % Display the matches
-figure(6) ; clf ;
-plotmatches(im1,im2,frames1,frames2,matches_2nn) ;
+figure(5) ; clf ;
+plotMatches(im1,im2,frames1,frames2,matches_2nn) ;
 title('Matches filtered by the second nearest neighbour test') ;
 
 % --------------------------------------------------------------------
@@ -78,5 +83,5 @@ matches_geo = matches_2nn(:, inliers) ;
 
 % Display the matches
 figure(6) ; clf ;
-plotmatches(im1,im2,frames1,frames2,matches_geo) ;
+plotMatches(im1,im2,frames1,frames2,matches_geo) ;
 title('Matches filtered by geometric verification') ;
