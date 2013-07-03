fde5d63b25cc72538e9302cb0e1e0e9bb0f60ba2
mcimpoi <mircea@example.com>
2013-07-03 21:35:59 +0100

    Adding resizing to the feature extraction process; Fixing file reading and using the new folder structure (image names are 7 digits; stored in folders named as 4 digit prefix) Changed fields of imdb structure; will update to contain more metadata from the new text files; Experimented with hessian interest point detector (similar to last year's demo).
    vl_kmeans added and commented out --> fails with out of memory message.

--- exercise4.m
+++ exercise4.m
@@ -6,7 +6,7 @@ setup ;
 % Load the database if not already in memory, or if the one
 % is from exercise3.
 if ~exist('imdb', 'var') || ~isfield(imdb.images,'wikiName')
-  imdb = loadIndex('data/art_imdb_100k_disc_dog.mat', ...
+  imdb = loadIndex('data/art_imdb_100k_ellipse_hessian.mat', ...
                    'sqrtHistograms', true) ;
   imdb.dir = '' ; % art images are not shipped with practical
 end
@@ -15,14 +15,16 @@ end
 % can be a path to a file or a URL pointing to an image in the
 % Internet.
 
-url1 = 'data/queries/mistery-painting1.jpg' ;
-url2 = 'data/queries/mistery-painting2.jpg' ;
-url3 = 'data/queries/mistery-painting3.jpg' ;
-res = search(imdb, url1, 'box', []) ;
+url1 = 'data/queries/mistery-painting-01.jpg' ;
+url2 = 'data/queries/mistery-painting-09.jpg' ;
+url3 = 'data/queries/mistery-painting-11.jpg' ;
+res = search(imdb, url3, 'box', []) ;
 
 % Display the results
 figure(1) ; clf ; set(gcf,'name', 'Part IV: query image') ;
 plotQueryImage(imdb, res) ;
 
+imdb.dir = 'data/impressionists/paintings/';
+
 figure(2) ; clf ; set(gcf,'name', 'Part IV: search results') ;
-plotRetrievedImages(imdb, res, 'num', 9) ;
+plotRetrievedImages(imdb, res, 'num', 16) ;
--- extra/preprocess_impressionists.m
+++ extra/preprocess_impressionists.m
@@ -3,16 +3,17 @@ function preprocess_impressionists()
 %   PREPROCESS() download an image dataset into 'data/', VLFeat into
 %   'vlfeat/', and precompute the histograms for the dataset.
 
-  setup ;
+  %setup ;
 
   % --------------------------------------------------------------------
   %                                                      Setup Oxford 5k
   % --------------------------------------------------------------------
   %prefix = 'data/oxbuild' ;
   %imdb = setupOxford5kBase('data/oxbuild', 'data/oxbuild_images') ;
+  addpath('..');
   prefix = 'data/art' ;
   imdb = setupArtBase(prefix, 'data/impressionists') ;
-  for t = [1 3]
+  for t = 3 
     switch t
       case 1
         suffix = '100k_disc_dog' ;
@@ -23,9 +24,9 @@ function preprocess_impressionists()
         numWords = 100e3 ;
         featureOpts = {'method', 'dog', 'affineAdaptation', false, 'orientation', true} ;
       case 3
-        suffix = '100k_ellipse_dog' ;
+        suffix = '100k_ellipse_hessian' ;
         numWords = 100e3 ;
-        featureOpts = {'method', 'dog', 'affineAdaptation', true, 'orientation', false} ;
+        featureOpts = {'method', 'hessian', 'affineAdaptation', true, 'orientation', false} ;
       case 4
         suffix = '100k_oellipse_dog' ;
         numWords = 100e3 ;
@@ -39,19 +40,21 @@ end
 function imdb = setupArtBase(prefix, artDir)
 % --------------------------------------------------------------------
   imdbPath = [prefix '_imdb.mat'] ;
-  listPath = fullfile(artDir, 'filelist001.txt') ;
+  listPath = fullfile(artDir, 'impress_and_post.txt') ;
 
-  [f,msg]=fopen(listPath, 'r','n','UTF-8') ;
-  txt = fscanf(f,'%c') ;
+  f = fopen(listPath, 'r','n','UTF-8') ;
+  data = textscan(f, '%s%s%s%s','delimiter', '\t') ;
   fclose(f) ;
-  data = textscan(txt,'%s\t%s','whitespace','\t') ;
-  names = data{1} ;
-  wikiNames = data{2} ;
 
-  imdb.dir = fullfile(artDir,'paintings_resized') ;
-  imdb.images.id = 1:numel(names) ;
-  imdb.images.name = names ;
-  imdb.images.wikiName = wikiNames ;
+  localNames = data{1};
+  downloadLinks = data{2};
+  infoLinks = data{3};
+
+  imdb.dir = fullfile(artDir,'paintings') ;
+  imdb.images.id = 1:numel(localNames) ;
+  imdb.images.name = localNames ;
+  imdb.images.downloadLinks = downloadLinks;
+  imdb.images.infoLinks = infoLinks;
   save(imdbPath, '-STRUCT', 'imdb') ;
 end
 
@@ -70,20 +73,27 @@ function setupArt(imdb, prefix, suffix, numWords, featureOpts)
   numWordsPerImage = ceil(imdb.numWords * 10 / numel(imdb.images.name)) ;
   parfor i = 1:numel(imdb.images.name)
     fprintf('get features from %i, %s\n', i, imdb.images.name{i}) ;
-    [~,descrs{i}] = getFeatures(imread(fullfile(imdb.dir, imdb.images.name{i})), ...
-                                imdb.featureOpts{:}) ;
+
+    [~, descrs{i}] = getFeatures(imread(fullfile(imdb.dir, imdb.images.name{i}(1:4), ...
+        imdb.images.name{i})), imdb.featureOpts{:});
     randn('state',i) ;
-    descrs{i} = vl_colsubset(descrs{i},numWordsPerImage) ;
+    descrs{i} = vl_colsubset(descrs{i}, numWordsPerImage) ;
   end
 
   descrs = cat(2,descrs{:}) ;
-  [imdb.vocab, imdb.kdtree] = annkmeans(descrs, imdb.numWords, ...
-                                        'numTrees', 4, ...
-                                        'maxNumComparisons', 1024, ...
-                                        'maxNumIterations', 30, ...
-                                        'tolerance', 1e-3, ...
-                                        'verbose', true, ...
-                                        'seed', 2) ;
+
+% imdb.vocab = vl_kmeans(descrs, imdb.numWords, 'algorithm', 'elkan', ...
+%    'verbose') ;
+
+% imdb.kdtree = vl_kdtreebuild(imdb.vocab);
+
+    [imdb.vocab, imdb.kdtree] = annkmeans(descrs, imdb.numWords, ...
+                                          'numTrees', 4, ...
+                                          'maxNumComparisons', 1024, ...
+                                          'maxNumIterations', 30, ...
+                                          'tolerance', 1e-3, ...
+                                          'verbose', true, ...
+                                          'seed', 2) ;
 
   % --------------------------------------------------------------------
   %                                                 Compute the features
@@ -91,8 +101,8 @@ function setupArt(imdb, prefix, suffix, numWords, featureOpts)
   clear frames words ;
   parfor i = 1:numel(imdb.images.name)
     fprintf('get features from %i, %s\n', i, imdb.images.name{i}) ;
-    [frames{i},descrs] = getFeatures(imread(...
-      fullfile(imdb.dir, imdb.images.name{i})), imdb.featureOpts{:}) ;
+    [frames{i},descrs] = getFeatures(imread(fullfile(imdb.dir, imdb.images.name{i}(1:4), ...
+        imdb.images.name{i})), imdb.featureOpts{:}) ;
     words{i} = vl_kdtreequery(imdb.kdtree, imdb.vocab, descrs, ...
                               'maxNumComparisons', 1024) ;
   end
